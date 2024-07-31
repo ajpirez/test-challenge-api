88b1a486a8e5eca65f6b5703845ecfafa7eadca4
Alejandro Pirez <ajpirez1994@gmail.com>
2024-07-31 08:02:47 +0200

    feat(add test for deleting all users by ids):

--- src/schemas/user.schema.js
+++ src/schemas/user.schema.js
@@ -5,7 +5,7 @@ const schema = Joi.object({
     lastName: Joi.string().min(3),
     email: Joi.string().min(3).required(),
     password: Joi.string().min(3).required(),
-    age: Joi.number(),
+    age: Joi.number().min(6),
     grade: Joi.number().valid(1, 2, 3, 4, 5),
     rols: Joi.string()
 })
